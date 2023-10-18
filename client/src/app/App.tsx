d6e16e97e5f9fcdd6dd9e6c2adc065f405ad8a46
AndyGrigs <andygrigs88@gmail.com>
2023-10-18 12:25:47 +0200

    adding styles, dark and light mode

--- client/src/app/App.tsx
+++ client/src/app/App.tsx
@@ -1,55 +1,7 @@
 import { useState } from "react";
 
-const Navbar: React.FC = () => {
-  return (
-    <div className="navbar">
-      {/* Hier können Sie die Navigationsleiste gestalten */}
-      <nav>
-        <ul>
-          <li>
-            <a href="/">Startseite</a>
-          </li>
-          <li>
-            <a href="/über-uns">Über uns</a>
-          </li>
-          <li>
-            <a href="/kontakt">Kontakt</a>
-          </li>
-        </ul>
-      </nav>
-    </div>
-  );
-};
-
-const Main: React.FC = () => {
-  return (
-    <div className="main">
-      {/* Hier können Sie den Hauptinhalt Ihrer Website einfügen */}
-      <h1>Willkommen auf unserer Website</h1>
-      <p>Hier ist der Hauptinhalt der Website...</p>
-    </div>
-  );
-};
-
-const Footer: React.FC = () => {
-  return (
-    <div className="footer">
-      {/* Hier können Sie die Firmeninformationen einfügen */}
-      <p>Firmenname</p>
-      <p>Adresse</p>
-      <p>Kontaktinformationen</p>
-    </div>
-  );
-};
-
 const App: React.FC = () => {
-  return (
-    <div className="app">
-      <Navbar />
-      <Main />
-      <Footer />
-    </div>
-  );
+  return <div className="app"></div>;
 };
 
 export default App;
