252b23657d931f0d7e732168cf3151440c8e649a
AndyGrigs <andygrigs88@gmail.com>
2023-10-19 10:29:22 +0200

    setting up router-dom

--- client/src/app/App.tsx
+++ client/src/app/App.tsx
@@ -1,7 +1,12 @@
 import { useState } from "react";
+import AppRouter from "./Router/ui/AppRouter";
 
 const App: React.FC = () => {
-  return <div className="app"></div>;
+  return (
+    <div className="app">
+      <AppRouter />
+    </div>
+  );
 };
 
 export default App;
--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,9 +1,12 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./app/App.js";
+import { BrowserRouter } from "react-router-dom";
 import "./app/styles/index.scss";
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
   </React.StrictMode>
 );
