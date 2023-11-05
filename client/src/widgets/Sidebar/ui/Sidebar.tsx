daac7885fa6a98c16fe8aa125ec6dc4b5b701be5
AndyGrigs <97991761+AndyGrigs@users.noreply.github.com>
2023-11-05 21:10:09 +0100

    some chages

--- client/src/widgets/Sidebar/ui/Sidebar.tsx
+++ client/src/widgets/Sidebar/ui/Sidebar.tsx
@@ -1,38 +1,29 @@
-
 import React, { useState } from "react";
-import cls from "./Sidebar.module.scss"
-import { classNames } from "@/shared/lib/classNames"
-import ArrowRight from "@/assets/arrow-small-right-svgrepo-com.svg?react"
-import ArrowLeft from "@/assets/arrow-small-left-svgrepo-com.svg?react"
+import cls from "./Sidebar.module.scss";
+import { classNames } from "@/shared/lib/classNames";
+import ArrowRight from "@/assets/arrow-small-right-svgrepo-com.svg?react";
+import ArrowLeft from "@/assets/arrow-small-left-svgrepo-com.svg?react";
 import { useTheme } from "@/app/Theme/ui";
-import styled from 'styled-components';
-
-const StyledArrowLeft = styled(ArrowLeft)`
-  /* Your CSS styles here */
-  fiil: red;
-`;
-
-const StyledArrowRight = styled(ArrowRight)`
-  /* Your CSS styles here */
-`;
 
 export interface SidebarProps {
-    className?: string;
+  className?: string;
 }
 
 export const Sidebar = ({ className }: SidebarProps) => {
-    const [collapsed, setCollapsed] = useState(false)
-    const [theme] = useTheme();
+  const [collapsed, setCollapsed] = useState(false);
+  const [theme] = useTheme();
 
-    const toogle = () => {
-        setCollapsed(prev => !prev)
-    }
+  const toogle = () => {
+    setCollapsed((prev) => !prev);
+  };
 
-
-    return (
-        <div className={classNames(cls.Sidebar, { [cls.collapsed]: collapsed }, [className])}>
-            <StyledArrowLeft />
-            <ArrowRight />
-        </div>
-    )
+  return (
+    <div
+      className={classNames(cls.Sidebar, { [cls.collapsed]: collapsed }, [
+        className,
+      ])}
+    >
+      <ArrowRight />
+    </div>
+  );
 };
